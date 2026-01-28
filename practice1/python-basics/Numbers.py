913b480e6e14488cbf917833180a3659a7645e7a
wasteintime <zumalizanibek@gmail.com>
2026-01-28 22:32:11 +0500

    new edit

---
+++ practice1/python-basics/Numbers.py
@@ -0,0 +1,42 @@
+import random
+x1 = 3    # int
+y1 = 3.5  # float
+z1 = 3j   # complex
+
+x2 = 4    # int
+y2 = 4.0  # float
+z2= 4j   # complex
+
+x3 = 1    # int
+y3 = 1.0  # float
+z3 = 1j   # complex
+
+x4 = 6    # int
+y4 = 6.6  # float
+z4 = 6j   # complex
+
+x5 = 2    # int
+y5 = 2.0  # float
+z5 = 2j   # complex
+
+a = float(x5)
+
+b = int(y4)
+
+c = complex(x3)
+
+d = int(y1)
+
+e = complex(x2)
+
+
+
+print(random.randrange(1, 5))
+
+print(random.randrange(5, 10))
+
+print(random.randrange(10, 15))
+
+print(random.randrange(15, 20))
+
+print(random.randrange(20, 25))
