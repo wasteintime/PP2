2f37c0fb57cc9eafb4d92540bbaa7f22a2b1dcc2
wasteintime <zumalizanibek@gmail.com>
2026-01-28 22:46:47 +0500

    new drop

---
+++ practice1/python-basics/Strings.py
@@ -0,0 +1,27 @@
+# "Hello" and 'Hello' are the same string
+print("Hello")
+print('my')
+print("name")
+print('is')
+print("Zhanibek")
+
+print("You can call me 'Zhan'")
+print('or call me "Zhanik"')
+print("but don't call me Zheka")
+print('so call me please "Zhanik"')
+print('or call me simply "Zhan"')
+
+a1 = "My"
+print(a1)
+
+a2 = "surname"
+print(a2)
+
+a3 = "is"
+print(a3)
+
+a4 = "Zhumali"
+print(a4)
+
+a5 = "!!!"
+print(a5)
